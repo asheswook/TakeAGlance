ba09648667e336627f50d53c8a2347247e2f926b
asheswook <wookboy00@naver.com>
2023-05-30 16:39:49 +0900

    Frame 생성 함수 create, 파일 분리

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template, Response
-import cv2
-import numpy as np
-import time
 import datetime
-import sys
+from frame import *
+import VisageSnap
 
-faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-num = 3
 
 app = Flask(__name__)
-
+vs = VisageSnap.Core()
+vs.load_model()
+vs.set_label(["biden", "obama", "jaewook", "haehyeon"])
 
 @app.route('/')
 def index():
@@ -21,59 +19,6 @@ def index():
         'time': timeString
     }
     return render_template('index.html', **templateData)
-
-
-@app.route('/train')
-def train():
-    """image training page"""
-    
-    return render_template('train.html')
-
-
-def gen_frames():
-    camera = cv2.VideoCapture(0)
-    width = camera.get(cv2.CAP_PROP_FRAME_WIDTH)
-    height = camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    fourcc = cv2.VideoWriter_fourcc(*"XVID")
-    fps = 30
-
-    time.sleep(0.2)
-    lastTime = time.time()*1000.0
-    while True:
-        ret, image = camera.read()
-
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        faces = faceCascade.detectMultiScale(
-            gray, scaleFactor=1.1, minNeighbors=6)
-        delt = time.time()*1000.0-lastTime
-        s = str(int(delt))
-        # print (delt," Found {0} faces!".format(len(faces)) )
-        lastTime = time.time()*1000.0
-        # Draw a rectangle around the faces
-        for (x, y, w, h) in faces:
-            cv2.circle(image, (int(x+w/2), int(y+h/2)),
-                       int((w+h)/3), (255, 255, 255), 3)
-        cv2.putText(image, s, (10, 25), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.7, (0, 255, 0), 2)
-        now = datetime.datetime.now()
-        timeString = now.strftime("%Y-%m-%d %H:%M")
-        cv2.putText(image, timeString, (10, 45),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
-        # cv2.imshow("Frame", image)
-
-        key = cv2.waitKey(1) & 0xFF
-     # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
-            break
-
-        ret, buffer = cv2.imencode('.jpg', image)
-        frame = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        
-    camera.release()
-    cv2.destroyAllWindows()
 
 @app.route('/video_feed')
 def video_feed():
---
+++ frame.py
@@ -0,0 +1,65 @@
+import cv2
+import numpy as np
+import time
+import datetime
+import sys
+import face_recognition
+import os
+from app import *
+from VisageSnap import To
+
+
+def gen_frames():
+    camera = cv2.VideoCapture(0)
+    width = camera.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    fps = 30
+
+    time.sleep(0.2)
+    lastTime = time.time()*1000.0
+    
+    while True:
+        ret, frame = camera.read()
+
+        # 1/4 size
+        small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+        rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_locations = [(top*2, right*2, bottom*2, left*2) for top, right, bottom, left in face_locations]
+
+        face_names = []
+        for encoding in face_encodings:
+            prediction = vs.predict_encoding(encoding)
+            print(prediction)
+
+            name = vs.convert_labelType(prediction, To.NAME)
+            face_names.append(name)
+            print(name)
+
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            name = "Unknown" if name == None else name
+            cv2.circle(frame, (int((left+right)/2), int((top+bottom)/2)), int((right-left)/2), (255, 255, 255), 3)
+            cv2.putText(frame, name, (left+3, bottom+22), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+        now = time.time()*1000.0
+        fps = 1000.0/(now-lastTime)
+        lastTime = now
+        cv2.putText(frame, "FPS: "+str(int(fps)), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+        key = cv2.waitKey(1) & 0xFF
+
+     # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+        
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    
+    camera.release()
+    cv2.destroyAllWindows()
