c1581f91914f376ffb972f949f23f9a608638c64
kty1004 <ktyoung1004@gmail.com>
2023-05-31 22:23:10 +0900

    csv에 VS로 학습한 데이터 기입 유무 확인.
    기입 유무 확인 코드 작성함.

--- add_csv.py
+++ add_csv.py
@@ -1,32 +1,67 @@
 import csv
+  
+def csv_to_dict(): #csv data 전체를 디셔너리에 담음.
+    csv_data={}
+    with open('data.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader: # row는 list형태로 저장되어 있음.
+            if not bool(row) or row==['img_label','img_path']:
+                continue
+            else:
+                csv_data[row[0]]=row[1]
+                
+    
+    return csv_data
 
-def save_data_to_csv(img_label, img_path):
-    new_row=[img_label, img_path]
+def adding_data_to_csv(new_row: list):
     with open('data.csv', 'a', newline='', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(new_row)
-        csvfile.close()
+        if csvfile.tell() == 0:
+            writer.writerow(new_row)
+        else:
+            csvfile.write('\n')
+            writer.writerow(new_row)
+    
+    csv_to_dict()
+
+def save_data_to_csv(img_label, img_path): # 이름 좀 잘못 지었는데...
+    new_row=[img_label, img_path]
+    adding_data_to_csv(new_row)
         
 
-def reading_csv_Data():
+def csv_data_visualization():
     trs=[]
     with open('data.csv', 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
-        num=0 
+        num=0
         for row in csv_reader:
             if not row:
                 continue
             if num==0:
                 num+=+1 # csv의 head를 출력 안 하기 위함.
                 continue
-            href = '{{url_for(\'static\', filename={})}}'.format(row[1])
-            #tr_=f'<tr><th scope=row>{num}</th><td>{row[0]}</td><td<a href={row[1]}>{row[1]}</a></td></tr>'
-            tr_ = f'<tr><th scope=row>{num}</th><td>{row[0]}</td><td><a href="{{url_for(static, filename={row[1]})}}">{row[1]}</a></td></tr>'
+            href = '{{url_for(static, filename={})}}'.format(row[1])
+            tr_=f'<tr><th scope=row>{num}</th><td>{row[0]}</td><td>{row[1]}</td></tr>' # 그냥 파일 이름이 보임.
+            #tr_ = f'<tr><th scope=row>{num}</th><td>{row[0]}</td><td><a href="{{url_for(static, filename={row[1]})}}">{row[1]}</a></td></tr>'
 
             trs.append(tr_)
             num+=+1
     
     trs=' '.join(trs)
     return trs
+
+
+def vs_in_csv_confirm(vs_row: list, csv_data: dict): # vs 데이터가 csv에 있는지 확인 후 처리.
+    # csv data에 vs_data가 있는지 확인
+    if vs_row[0] not in list(csv_data.keys()) or vs_row[1] not in list(csv_data.values()): # vs 데이터가 존재하지 않을 때
+        adding_data_to_csv(vs_row)
+    else:# 데이터가 존재할 때
+        print(csv_data)
+        print(csv_data[vs_row[0]], 'add_csv')
+            
+
+        
     
     
+
+            
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import datetime
 from frame import *
 import VisageSnap
 import csv
-from add_csv import save_data_to_csv, reading_csv_Data
+from add_csv import save_data_to_csv, csv_data_visualization, vs_in_csv_confirm, csv_to_dict
 
 
 app = Flask(__name__)
@@ -14,7 +14,6 @@ vs.set_label(["biden", "obama", "jaewook", "haehyeon"])
 
 UPLOAD_FOLDER = 'image'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 
 @app.route('/')
 def index():
@@ -37,16 +36,24 @@ def video_feed():
 @app.route('/train', methods=['GET', 'POST'])
 def train():
     # csv 파일 데이터 확인
-    trs=reading_csv_Data()
+    csv_data=csv_to_dict() # csv 데이터를 dict으로 가져오기.
+    for get_face in vs.get_faces(): # vs로 학습한 데이터가 csv데이터에 있는지 확인 이후 추가 유무 확인.
+        vs_row=[get_face.label, get_face.filenames[0]]
+        vs_in_csv_confirm(vs_row, csv_data=csv_data)
+        
+    trs=csv_data_visualization() # html 파일 table에 값 형성.
     
     if request.method=='POST':
-        print('post method activated')
+        
         img_label=request.form['label']
         img_file=request.files['image']
         image_path='static/images/' + img_file.filename
         img_file.save(image_path)
-        save_data_to_csv(img_label=img_label, img_path=image_path)
-        trs=reading_csv_Data()
+        if img_label in list(csv_data.keys()) or image_path in list(csv_data.values()): # 이미 csv에 있을 때.
+            trs=csv_data_visualization()  # çsv에 저장은 안하고 그냥 csv 파일만 보여줌.
+        else:
+            save_data_to_csv(img_label=img_label, img_path=image_path)
+            trs=csv_data_visualization()
         return render_template('train.html', trs=trs) # 그냥 전체 re-rendering 함.
         
     
