a6f831f4fb6df61328a9a0943430944635417325
asheswook <wookboy00@naver.com>
2023-06-01 10:47:23 +0900

    Image Frame 무한 로딩 픽스.

--- static/js/apps.js
+++ static/js/apps.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var loadingMessage = document.getElementById('loading-message');
-    var videoStream = document.getElementById('video-stream');
-    var statusMessage = document.getElementById('status-message');
-
-    videoStream.addEventListener('load', function() {
-        loadingMessage.style.display = 'none';
-        
-        statusMessage.style.display = 'block';
-    });
-    
+document.addEventListener("DOMContentLoaded", function () {
+  var loadingMessage = document.getElementById("loading-message");
+  var statusMessage = document.getElementById("status-message");
+  var frame = document.getElementById("frame");
+  var image = new Image();
+  image.src = "/video_feed";
+  image.id = "video-stream";
+  image.addEventListener("load", function () {
+    frame.innerHTML = "";
+    frame.appendChild(image);
+    loadingMessage.style.display = "none";
+    statusMessage.style.display = "block";
+  });
 });
